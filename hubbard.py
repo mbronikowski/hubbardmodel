Fixed Hamiltonian generation signs

hubbard.py
@@ -293,23 +293,28 @@ def list_possible_spinless_square_hops(vector, number_of_electrons, side_size):
     number_of_sites = side_size ** 2
     resulting_vectors = np.empty((2, 4 * number_of_electrons + 1), dtype=int)
     resulting_vectors[0][0] = 0  # The 0th element holds the number of found hops.
+    no_of_source_el = 0
     for source_bit in range(number_of_sites):
         if vector >> source_bit & 1:
+            no_of_source_el += 1
             hops = square_hop_lookup[source_bit] & ~ vector
+            no_of_passed_els = 0
             for hop_bit in range(number_of_sites):
+                if (vector >> hop_bit & 1) and hop_bit != source_bit:
+                    no_of_passed_els += 1
                 if hops >> hop_bit & 1:
                     resulting_vectors[0][0] += 1
                     resulting_vectors[0][resulting_vectors[0][0]] = (vector | (1 << hop_bit)) & ~ (2 ** source_bit)
-                    if (source_bit - hop_bit) % 2:
-                        resulting_vectors[1][resulting_vectors[0][0]] = -1
+                    if (no_of_source_el - no_of_passed_els) % 2:
+                        resulting_vectors[1][resulting_vectors[0][0]] = -2
                     else:
-                        resulting_vectors[1][resulting_vectors[0][0]] = 1
+                        resulting_vectors[1][resulting_vectors[0][0]] = 2
     result_view = resulting_vectors[:, 1: resulting_vectors[0][0] + 1]
     return result_view[:, result_view[0, :].argsort()]
 
 
 def spinless_square_hamiltonian(basis, number_of_electrons, side_size):
-    hamiltonian = sparse.dok_matrix((basis.size, basis.size), dtype=np.double)
+    hamiltonian = sparse.dok_matrix((basis.size, basis.size), dtype=np.float)
     it_basis = np.nditer(basis, flags=['f_index'])
     while not it_basis.finished:
         vecs_to_add = list_possible_spinless_square_hops(basis[it_basis.index], number_of_electrons, side_size)
@@ -348,7 +353,7 @@ def list_possible_free_square_hop_indices(basis, vector, number_of_positive_spin
 
 def free_square_hamiltonian(basis, number_of_electrons, number_of_positive_spins, side_size):
     number_of_negative_spins = number_of_electrons - number_of_positive_spins
-    hamiltonian = sparse.dok_matrix((basis[:, 0].size, basis[:, 0].size), dtype=np.double)
+    hamiltonian = sparse.dok_matrix((basis[:, 0].size, basis[:, 0].size), dtype=np.float)
     it_basis = np.nditer(basis[:, 0], flags=['f_index'])
     while not it_basis.finished:
         vecs_to_add = list_possible_free_square_hop_indices(basis, basis[it_basis.index],
@@ -394,7 +399,7 @@ def list_possible_constrained_square_hop_indices(basis, vector, number_of_positi
 
 def constrained_square_hamiltonian(basis, number_of_electrons, number_of_positive_spins, side_size):
     number_of_negative_spins = number_of_electrons - number_of_positive_spins
-    hamiltonian = sparse.dok_matrix((basis[:, 0].size, basis[:, 0].size), dtype=np.double)
+    hamiltonian = sparse.dok_matrix((basis[:, 0].size, basis[:, 0].size), dtype=np.float)
     it_basis = np.nditer(basis[:, 0], flags=['f_index'])
     while not it_basis.finished:
         vecs_to_add = list_possible_constrained_square_hop_indices(basis, basis[it_basis.index],
@@ -448,7 +453,7 @@ def spectral_green_lanczos(model, abs_em_type, ref_vec_with_norm, ground_state_e
 
     last_i = ref_vec_current.shape[0] - 1
     for i in range(ref_vec_current.shape[0]):
-        norm_energy_array[i][0] = norm_current ** 2
+        norm_energy_array[i][0] = norm_current # Why are we squaring this?  ** 2
         ref_vec_multiplied_by_hmltn = model.multiply_vec_hmltn(ref_vec_current)
         energy_diff = (np.vdot(ref_vec_current, ref_vec_multiplied_by_hmltn) - ground_state_energy).real
         # Casting to real only deletes numerical residue.